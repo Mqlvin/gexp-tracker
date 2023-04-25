e2f7953eda3bfd0d32ec9c8a1e3baec10e956869
Mqlvin <pricklydotcactus@gmail.com>
2023-04-25 20:27:16 +0000

    Update test command

--- src/commands/impl/command_test.ts
+++ src/commands/impl/command_test.ts
@@ -1,8 +1,9 @@
 import { Message } from "discord.js";
 import { Command } from "../abstract_command";
+import { getUUID } from "../../api/player_db";
 
 export class TestCommand extends Command {
-    onExecute(args: string[], discordMsg: Message): void {
-        console.log("executed test command with: " + args)
+    async onExecute(args: string[], discordMsg: Message): Promise<void> {
+        discordMsg.reply("UUID: " + await getUUID(args[0]));
     }
 }
