661f02804127a5b7a1d51e7b17592c8f6cdd5b8c
Mqlvin <pricklydotcactus@gmail.com>
2023-04-30 09:19:16 +0100

    Add guild endpoint requesting

--- src/api/hypixel.ts
+++ src/api/hypixel.ts
@@ -1,5 +1,21 @@
-import { createFile } from "../filesystem/files";
+import { startRequesting } from "./player_handler/request_scheduler";
+import { getJson } from "./requester";
+
+const GUILD_ENDPOINT: string = "https://api.hypixel.net/guild?key=%KEY%&id=%ID%"
+
 
 export function hypixelStartup(): void {
+    startRequesting();
+}
+
+export async function getGuildData(key: string, guildId: string): Promise<any | undefined> {
+    let formattedEndpoint = GUILD_ENDPOINT.replace("%KEY%", key).replace("%ID%", guildId);
+
+    let response: any = await getJson(formattedEndpoint, {});
+
+    return response;
+}
+
+function saveGuildData(): void {
     
 }
