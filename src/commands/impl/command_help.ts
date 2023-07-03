db4dbdb1aa19fb64e2616c8bdbe78b30a783511b
Mqlvin <pricklydotcactus@gmail.com>
2023-07-03 20:28:03 +0100

    Update the help command

--- src/commands/impl/command_help.ts
+++ src/commands/impl/command_help.ts
@@ -6,10 +6,13 @@ export class HelpCommand extends Command {
     async onExecute(args: string[], discordMsg: Message): Promise<void> {
         let embed: EmbedBuilder = new EmbedBuilder()
 	            .setColor(0x3FB6B6)
-	            .setTitle("Monthly player ranks") // `\u1CBC` is invisible character
+	            .setTitle("Help command") // `\u1CBC` is invisible character
 	            .setDescription(
-                    "`!lb <daily/weekly/monthly>` - Displays a GEXP leaderboard for the specified timeframe.\n" +
-                    "`!tiers` - Displays guild members categorised by the tier they've achieved."
+                    "`g!help` - Displays this message.\n" +
+                    "`g!lb <daily/weekly/monthly>` - Displays a GEXP leaderboard for the specified timeframe.\n" +
+                    "`g!tiers` - Displays guild members categorised by the tier they've achieved.\n" +
+                    "`g!player <username>` - Displays monthly GEXP information for the specified player.\n" + 
+                    "`g!vets` - Displays a list of veterans and should-be veterans."
                 )
 	            .setTimestamp(new Date())
 	            .setFooter({ text: "GEXP Tracker" });
